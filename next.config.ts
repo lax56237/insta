d04239ab797f09e87a3c3774d604ce52caf27154
lax <lax56237@gmail.com>
2025-11-28 22:37:10 +0530

    config 2

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,27 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-
-};
-
-export default nextConfig;
-
-
-
-
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   output: "standalone",
-//   experimental: {},
-//   webpack: (config, { isServer }) => {
-//     // This disables Turbopack completely
-//     config.infrastructureLogging = { level: "none" };
-//     return config;
-//   },
+//   /* config options here */
+
 // };
 
 // export default nextConfig;
+
+
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  experimental: {},
+  webpack: (config, { isServer }) => {
+    // This disables Turbopack completely
+    config.infrastructureLogging = { level: "none" };
+    return config;
+  },
+};
+
+export default nextConfig;
 
 
